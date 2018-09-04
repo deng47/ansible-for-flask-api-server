8391a690e0b7d603b1e43e3ea43021da4cd3d9e0
siqi deng <deng47@gmail.com>
2018-09-04 08:39:13 -0700

    Update api.py
    Rename variables

--- files/api.py
+++ files/api.py
@@ -7,29 +7,29 @@ app = Flask(__name__)
 
 @app.route('/api/MemFree')
 def getfreemem():
-    with open('/proc/meminfo', 'r') as m:
-         freemem = m.readlines()[1]
+    with open('/proc/meminfo', 'r') as f:
+         freemem = f.readlines()[1]
          return jsonify(MemFree=re.findall(r'\d+ kB', freemem)[0])
 
 
 @app.route('/api/SReclaimable')
 def getrecmem():
-    with open('/proc/meminfo', 'r') as m:
-         meminfo = m.readlines()[22]
+    with open('/proc/meminfo', 'r') as f:
+         meminfo = f.readlines()[22]
          return jsonify(SReclaimable=re.findall(r'\d+ kB', meminfo)[0])
 
 
 @app.route('/api/loadavg')
 def getload():
-    with open('/proc/loadavg', 'r') as m:
-         loadavg = m.read().split()
+    with open('/proc/loadavg', 'r') as f:
+         loadavg = f.read().split()
          return jsonify(loadavg=loadavg[0:3])
 
 
 @app.route('/api/partitions')
 def getpart():
-    with open('/proc/partitions', 'r') as m:
-         partitions = [ each.split()[-1] for each in m.readlines()[2:] ]
+    with open('/proc/partitions', 'r') as f:
+         partitions = [ each.split()[-1] for each in f.readlines()[2:] ]
          return jsonify(Partitions=partitions)
 
 
